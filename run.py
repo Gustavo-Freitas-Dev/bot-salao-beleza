2a34efe83175ce4c988a786b8150c31c725dce57
Gustavo Freitas <gustavoluis2163k@gmail.com>
2025-12-17 22:21:06 -0300

    Atualiza requirements.txt e run.py para deploy no Railway

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
